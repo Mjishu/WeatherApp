8c966381b01bc09e1af244651fb8ab0c43336ce3
Mjishu <128941980+Mjishu@users.noreply.github.com>
2023-12-08 13:18:22 -0500

    Everything is done besides the error message if you input the wrong name

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ let unitWind = "kph"
 
 let temperatureLocaiton = 'jacksonville';
 
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  };
+
 async function getDayForecast(){
     const weatherLocationApi = await fetch(`http://api.weatherapi.com/v1/current.json?key=efc06b8840d54d3d941183403230512&q=${temperatureLocaiton}`, {mode:'cors'});
     if(weatherLocationApi.ok){
@@ -15,10 +25,17 @@ async function getDayForecast(){
 
         console.log(weatherData)
         roundedCurrentTemp = Math.round(weatherData.current[`temp_${unit}`])
-    
-        holder.innerHTML = `Weather in ${weatherData.location.name}, ${weatherData.location.region} is ${roundedCurrentTemp}° ${unit.toUpperCase()} <br> Local Time is ${weatherData.location.localtime}`;
-        mainTemp.innerHTML = roundedCurrentTemp + "°" + unit.toUpperCase();
+        
+
+        const localTimeStr = weatherData.location.localtime;
+        const localTimeDate = new Date(localTimeStr);
+        const timeFormat = Intl.DateTimeFormat('en-US',options).format(localTimeDate);
+
         cloudImg.src = weatherData.current.condition.icon;
+        holder.innerHTML = `${weatherData.location.name}, ${weatherData.location.region}<br>${timeFormat}`;
+        mainTemp.innerHTML = roundedCurrentTemp + "°" + unit.toUpperCase();
+        mainTemp.className = "mainTemp"
+        
         
         
     }
@@ -42,7 +59,7 @@ async function getDailyForecast(){
         const dailyForecast = document.createElement("div");
         dailyForecast.className = "dailyForecast";
         dailyForecast.setAttribute("id", 'day'+ (i+1));
-        dailyForecast.innerHTML = `${dailyData.forecast.forecastday[i].date} Temp ${avgRoundedTemp}° ${unit.toUpperCase()} <span class="minTemp">${minRoundedTemp}° ${unit.toUpperCase()}</span>`
+        dailyForecast.innerHTML = `${dailyData.forecast.forecastday[i].date} <br>${avgRoundedTemp}° ${unit.toUpperCase()} <br><span class="minTemp">${minRoundedTemp}° ${unit.toUpperCase()}</span>`
         
         const dailyImg = document.createElement("img");
         dailyImg.src = dailyData.forecast.forecastday[i].day.condition.icon;
@@ -52,9 +69,12 @@ async function getDailyForecast(){
         const feelslikeRoundedTemp = Math.floor(dailyData.current[`feelslike_${unit}`])
 
         const extraTemp = document.getElementById("extraTemp");
-        extraTemp.innerHTML = `<span>Feels ${feelslikeRoundedTemp} ${unit.toUpperCase()} </span><br> <span>Humidity ${dailyData.current.humidity}% </span><br><span> Wind ${dailyData.current[`wind_${unitWind}`]} ${unitWind} </span><br> <span>Chance of Rain ${dailyData.forecast.forecastday[i].hour[12].chance_of_rain}%</span>`
+        extraTemp.innerHTML = `<span class='spanEx'><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABdklEQVR4nO2XsUoDQRRFD4F0QUitraWKoBJF8Q9ik2/SKqB22mqhRT4gP6IhVtooSEBjoSjsysII4TKJOxJ8Y5wDt8nMhfv2zTwykEgkpski0AGGQC4qfrsE5ok4/MATXPUYaxGdEuG/dEGEDAMKeCJCclHoujmpAGtmrgN5oMxJBVgzcx1QoguspAKs+XcdyGMrKBVgzZ/uwPoPCig8UbA75jWm6HrhKbymLHge8h9A27O37dZG9w6sH/hdCXQP7EzYX6w9iKeLEasS5B3YLuFrAG/iXcOAUwlx8kveqXEjITYDvFvi7WOAHoN6gLcu3lcM0NFZC/DOifcZA3oSYiPA2xBvDwPOJcRhgPdYvGcY0PKc46USvhXP/WlhQNVNj9Egt98UsQzceSZQFSP2gEwCFV/3yJ3zmlPDHRv98hnQxJiDEn+Vx2mfCKi4INqJScqcp0JEND13wqd+DMdmHFU3UYoRew28OF25UdmyvLCJRCLB1PkEBvCEH+c7OMEAAAAASUVORK5CYII="> ${feelslikeRoundedTemp} ${unit.toUpperCase()} </span><br> <span class='spanEx'>        <img width="30" height="30" src="https://img.icons8.com/material-rounded/24/humidity.png" alt="humidity"/>
+        ${dailyData.current.humidity}% </span><br><span class='spanEx'>  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/wind--v1.png" alt="wind--v1"/> ${dailyData.current[`wind_${unitWind}`]} ${unitWind} </span><br> <span class='spanEx'> <img width="50" height="50" src="https://img.icons8.com/ios/50/heavy-rain.png" alt="heavy-rain"/> ${dailyData.forecast.forecastday[i].hour[12].chance_of_rain}%</span>`
 
     }
+    const tempConvert = document.getElementById("converter");
+    tempConvert.innerHTML = `Display °${unit.toUpperCase()}`
 }
 
 getDayForecast();
@@ -91,4 +111,4 @@ function changeUnit(){
 
 
 //todo: figure out how to make date look prettier
-//todo: if value = true then temperature = celsius, if false temperature = farenheit
+//todo: need to add error handler
